5818092c398fc07f35331f054360f05c63a245d0
Nicolas Choquet <nicolas.choquet.ext@orange.com>
2020-03-13 14:36:00 +0100

    change framework namespace

--- src/classes/commands/Register.php
+++ src/classes/commands/Register.php
@@ -6,6 +6,7 @@ namespace app\classes\commands;
 
 class Register extends \dis\core\classes\commands\Register {
     public static function set_commands() {
+        parent::set_commands();
         static::$commands = [
             ...static::$commands,
             'my-command' => MyCommand::class,
